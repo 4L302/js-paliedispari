d814b562906bdaee0dbebe23e9a8958da3060d9c
4L302 <alegarg2002@gmail.com>
2023-03-22 17:05:03 +0100

    complete

---
+++ palindroma/main.js
@@ -0,0 +1,27 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Consigli del giorno
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti. */
+
+const parola = prompt ('Inserisci la tua parola:');
+
+const parolaDivisa = parola.split('');
+console.log(parolaDivisa);
+const parolaDivisa = parolaDivisa.reverse();
+console.log(parolaDivisa);
+const parolaInvertita = parolaDivisa.join('');
+console.log(parolaInvertita);
+
+if(parola == parolaInvertita){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+}
